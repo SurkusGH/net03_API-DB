683bf150c0e7a76b5fd784970d5399b009f58a18
SurkusGH <v.surkus@me.com>
2022-06-16 06:21:08 +0300

    Update CarControlerTests.cs

--- net03_API-DB.Tests/CarControlerTests.cs
+++ net03_API-DB.Tests/CarControlerTests.cs
@@ -4,27 +4,26 @@ using net03_API_DB.Controllers;
 using net03_API_DB.Models;
 using net03_API_DB.Repositories;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace net03_API_DB.Tests
 {
     public class CarControlerTests
     {
-        [Theory, AutoData] // Fact reiðkia, kad testas neturi param
-        public void CarControler_GetCarByID_Returns_Not_Found_When_Repository_Returns_Null(Car car)
+        [Theory, AutoData] // [Fact] means that test does not have any params
+        public void CarControler_GetCarsByColor_Returns_Not_Found_When_Repository_Returns_Null(IEnumerable<Car> cars)
         {
             // Arrange
             var repositoryMock = new Mock<ICarRepository>();
-
             // Subject under test
             var sut = new CarController(repositoryMock.Object);
-            repositoryMock.Setup(x => x.GetCarsByColor(It.IsAny<string>())).Returns((System.Collections.Generic.IEnumerable<Car>)car);
+            repositoryMock.Setup(x => x.GetCarsByColor(It.IsAny<string>())).Returns(cars);
             sut.GetCarsByColor(It.IsAny<string>());
-
             //Act
             var response = sut.GetCarsByColor(It.IsAny<string>());
             //Assert
-            Assert.Equal((System.Collections.Generic.IEnumerable<Car>)car, response);
+            Assert.Equal(cars, response);
         }
     }
 }
